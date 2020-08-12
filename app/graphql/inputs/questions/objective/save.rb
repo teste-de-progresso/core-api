6857474b94e1c8cde5fddbfed150407775281925
João Geonizeli <joao.geonizeli@gmail.com>
2020-08-12 19:04:47 -0300

    add user_id argument

--- app/graphql/inputs/questions/objective/save.rb
+++ app/graphql/inputs/questions/objective/save.rb
@@ -17,6 +17,8 @@ module Inputs
         argument :authorship_year, String, required: false
         argument :source, String, required: false
         argument :own, Boolean, required: false
+
+        argument :user_id, Integer, required: false
       end
     end
   end
