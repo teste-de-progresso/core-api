bb2588e8d8d523d00736b210b33575781d2e96c8
João Geonizeli <joao.geonizeli@gmail.com>
2020-09-07 22:36:30 -0300

    rename querys and improve question pagination

---
+++ app/graphql/inputs/questions/objective_where.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Inputs
+  module Questions
+    class ObjectiveWhere < Types::BaseInputObject
+      graphql_name 'OjectiveWhereInput'
+
+      argument :check_type, Types::Questions::CheckType, required: false
+      argument :status, Types::Questions::Status, required: false
+      argument :difficulty, Types::Questions::Difficulty, required: false
+      argument :bloom_taxonomy, Types::Questions::BloomTaxonomy, required: false
+      argument :authorship_year, String, required: false
+      argument :own, Boolean, required: false
+      argument :user_id, Integer, required: false
+    end
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,19 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :search_objective_questions,
-          [Questions::Objective],
-          null: true,
-          description: 'Search by objective questions by body or status.' do
-            argument :page, Integer, required: true
-            argument :limit, Integer, required: true
-            argument :user_id, Integer, required: true
-          end
+    field :objective_questions, Questions::ObjectiveCollection, null: false
 
-    field :get_objective_question,
-          Questions::Objective,
-          null: true,
-          description: 'Get one objective question by ID.' do
-            argument :id, ID, required: true
-          end
-
-    field :my_user, Users::Details, null: false,
-                                    description: 'Get you current user Details.'
-
-    def search_objective_questions(user_id:, page:, limit:)
-      Objective.where(user_id: user_id).page(page).per(limit)
+    field :objective_question, Questions::Objective, null: true do
+      argument :id, ID, required: true
     end
 
-    def get_objective_question(id:)
+    field :my_user, Users::Details, null: false
+
+    def objective_questions
+      Objective.all
+    end
+
+    def objective_question(id:)
       Objective.find_by(id: id)
     end
 
