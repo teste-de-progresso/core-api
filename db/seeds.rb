0ee405b996bac3b734f094cf907204215e6a2fe5
João Geonizeli <joao.geonizeli@gmail.com>
2020-09-20 19:38:20 -0300

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
-roles = %i[
-  teacher
-  nde
-  coordinator
-  center_director
-  pro_rector
-]
-
-roles.each do |role|
-  user = User.create(email: "#{role}@example.com", password: 'password')
-  role = Role.create(name: role)
-
-  user.roles << role
-end
+`rails dev:users`
+`rails dev:questions`
