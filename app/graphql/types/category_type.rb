049524cbbbca7217a73042d0b071a31f9a8951d1
João Geonizeli <joao.geonizeli@gmail.com>
2020-11-04 22:09:32 -0300

    fix graphql types

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -4,5 +4,6 @@ module Types
   class CategoryType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :subjects, [SubjectType], null: false
   end
 end
