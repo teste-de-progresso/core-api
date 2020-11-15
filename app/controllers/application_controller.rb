5a14c1b9011ad914a9373b5ba512e7919a919e23
João Geonizeli <joao.geonizeli@gmail.com>
2020-11-15 13:17:33 -0300

    fix rubocop linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    return User.first
     token = request.headers['Authorization']&.gsub(/^Bearer /, '')
     secret = Rails.env.production? ? ENV['DEVISE_JWT_SECRET_KEY'] : '1cb26f40-498b-4f72-a00a-e8633abc5957'
     user_id = JWT.decode(token, secret, true, algorithm: 'HS256', verify_jti: true).first['user_id']
