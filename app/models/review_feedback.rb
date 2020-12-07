31407a6f9f90daed0314f5d8c49bfcfa3aea785d
João Geonizeli <joao.geonizeli@gmail.com>
2020-12-07 00:02:21 -0300

    add review feedback status model enum

--- app/models/review_feedback.rb
+++ app/models/review_feedback.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ReviewFeedback < ApplicationRecord
+  extend Enumerize
+
   belongs_to :review_request
   has_one :user, through: :review_request
+
+  enumerize :status, in: %i[comment approve request_change]
 end
