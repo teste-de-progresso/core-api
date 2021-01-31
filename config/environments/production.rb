7d21b1903c906e09b3a4b3f7824825841598e05b
João Geonizeli <joao.geonizeli@gmail.com>
2021-01-31 14:30:58 -0300

    config email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,16 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    authentication: :plain,
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
+    domain: ENV['MAILGUN_DOMAIN'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD']
+  }
+
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
