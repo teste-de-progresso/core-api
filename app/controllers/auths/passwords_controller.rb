ad1628722aa8faf605057f0153512de852b18594
João Geonizeli <joao.geonizeli@gmail.com>
2021-02-07 13:14:12 -0300

    restore password by token

--- app/controllers/auths/passwords_controller.rb
+++ app/controllers/auths/passwords_controller.rb
@@ -15,5 +15,19 @@ module Auths
         respond_with(resource, status: :not_acceptable)
       end
     end
+
+    def update
+      self.resource = resource_class.reset_password_by_token(resource_params)
+
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        resource.unlock_access! if unlockable?(resource)
+
+        respond_with(resource, status: :ok)
+      else
+        respond_with(resource, status: :not_acceptable)
+      end
+    end
   end
 end
