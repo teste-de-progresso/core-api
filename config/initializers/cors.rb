64ce1af2f66c49cff2d70c657fc560310d910b2f
João Geonizeli <joao.geonizeli@gmail.com>
2021-04-28 10:38:16 -0300

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,10 +10,6 @@
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
     origins "*"
-    resource "*",
-      headers: %w[Authorization],
-      methods: :any,
-      expose: %w[Authorization],
-      max_age: 600
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
