a0758dbce1184580a1b1b84f17fccfb544d41a32
João Geonizeli <joao.geonizeli@gmail.com>
2021-06-09 21:29:08 -0300

    fix question policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
-
 class ApplicationPolicy
-  attr_reader :user, :record
+  attr_reader :user, :record, :roles
 
   def initialize(user, record)
     @user = user
     @record = record
+    @roles = user.roles.map { |r| r.name.to_sym }
   end
 
   def is?(role)
-    @user.roles.any? { |r| r.name.underscore.to_sym == role }
+    @roles.any?(role)
   end
 
   class Scope
