ed0034c279b6eddc9470fe97ffc443356d88ece5
João Geonizeli <joao.geonizeli@gmail.com>
2021-07-26 00:05:34 -0300

    add targeted knowledge field to question

--- app/graphql/inputs/question_create_input.rb
+++ app/graphql/inputs/question_create_input.rb
@@ -10,6 +10,7 @@ module Inputs
     argument :references, String, required: true
     argument :authorship_year, String, required: true
     argument :source, String, required: true
+    argument :targeted_knowledge, String, required: true
 
     argument :status, Types::Questions::Status, required: true
     argument :check_type, Types::Questions::CheckType, required: false
