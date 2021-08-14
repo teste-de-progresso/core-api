445540a9bcd342f7b33042816a2d8b8111fb44a4
João Geonizeli <joao.geonizeli@gmail.com>
2021-08-14 16:15:55 -0300

    remove nullables from BaseConnection

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -2,9 +2,11 @@
 module Types
   class BaseConnection < Types::BaseObject
     include GraphQL::Types::Relay::ConnectionBehaviors
+    has_nodes_field(false)
+    edges_nullable(false)
+    edge_nullable(false)
 
     field :total_count, Integer, null: false
-
     def total_count
       object.items.count
     end
